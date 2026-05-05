464390a83439d21ecb39fd6a6946ea24b8ee1348
Yash11778 <dev@barodynamics.in>
2026-05-05 14:30:18 +0530

    Fixedd tiger graph and other stuff

--- graphrag-hackathon/pipelines/utils.py
+++ graphrag-hackathon/pipelines/utils.py
@@ -16,7 +16,7 @@ _client: Groq | None = None
 def setup_groq() -> Groq:
     global _client
     if _client is None:
-        api_key = os.getenv('GROQ_API_KEY', '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')
+        api_key = os.getenv('GROQ_API_KEY', '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')
         _client = Groq(api_key=api_key)
     return _client
 
